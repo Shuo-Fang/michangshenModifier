c71dee9016d5588ca0aafe2a010ac72a5d8f29ce
Shuofang <1015510750@qq.com>
2020-10-30 16:08:47 +0800

    导入模板

---
+++ main.cpp
@@ -0,0 +1,15 @@
+﻿#include "mainwindow.h"
+#include <globaldef.hpp>
+#include <QApplication>
+
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    MainWindow mainwindow;
+    LOADQSS(GlobalSpace::STYLE_QSS_FILE_PATH);
+    mainwindow.showNormal();
+
+    return a.exec();
+}
